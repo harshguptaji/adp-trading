a2637fa22d6a9f5c1251ef000867ee5a02006bf1
harshguptaji <91708017+harshguptaji@users.noreply.github.com>
2024-10-02 15:16:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,28 @@
 
 import './App.css';
-import Reach from './Components/Reach';
-import Choose from './Components/Choose';
-import Work from './Components/Work';
-import About from './Components/About';
-import Application from './Components/Application';
-import Blog from './Components/Blog';
-import Company from './Components/Company';
-import Contact from './Components/Contact';
-import Distribution from './Components/Distribution';
-import Distributors from './Components/Distributors';
-import FAQ from './Components/FAQ';
-import Feedback from './Components/Feedback';
-import Finder from './Components/Finder';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
 import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Key from './Components/Key';
+
 import Navbar from './Components/Navbar';
+import ExportImport from './pages/ExportImport';
+
 
 function App() {
   return (
-   <>
-    <Navbar/>
-    <Header/>
-    <About/>
-    <Work/>
-    <Distributors/>
-    <Finder/>
-    <Choose/>
-    <Reach/>
-    <Application/>
-    <Company/>
-    <Distribution/>
-    <Key/>
-    <Blog/>
-    <Contact/>
-    <FAQ/>
-    <Feedback/>
+    <Router>
+    <div>
+    <Navbar/> {/* Navbar will always be visible */}
+      <div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/export-import" element={<ExportImport/>} />
+        </Routes>
+      </div>
     <Footer/>
-   </>
+    {/* Footer will always be visible */}
+    </div>
+  </Router>
   );
 }
 
